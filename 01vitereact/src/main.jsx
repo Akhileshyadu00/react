f3ec8dc3a23e946496362b02fe0fe8fd1cc7ea8d
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-03-25 17:36:51 +0530

    react-render

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,12 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import React from 'react';
+//import App from './App.jsx'
+
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank"
+//   },
+//   children: "Click to google"
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target= "_blank"> Google</a>
+)
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank"
+  },
+ " click to visit"
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    reactElement
+
 )
 
 //tag name  Upper
