814444cb556655b12c9999c55a1a850964209308
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-04-05 14:54:11 +0530

    mini context

---
+++ minicontext/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+     <h1>React With Mini Context</h1>
+
+     <Login />
+     <Profile />
+
+    </UserContextProvider>
+  )
+}
+
+export default App
