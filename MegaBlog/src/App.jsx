87f244e499c3d5c938c42c418a49a2cf259855f2
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-04-21 18:51:47 +0530

    Mega1

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL="test environment");
+  
+
+  return (
+    <>
+      <h1>Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
