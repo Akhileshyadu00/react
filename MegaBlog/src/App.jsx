0658c876a964f50db2f420afaf9ddb85cb11ae9f
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-04-22 17:36:48 +0530

    app&correction

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,16 +1,42 @@
-
-
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+
+
+
+
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL="test environment");
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  //console.log(import.meta.env.VITE_APP_APPWRITE_URL);
   
 
-  return (
-    <>
-      <h1>Blog App with Appwrite</h1>
-    </>
-  )
+  return !loading ? (<div className="min-h-sc bg-green-400 ">
+    <div className="w-full block"></div>
+    <Header/>
+    <main>
+      TODO: {<outlet/>}
+    </main>
+    <Footer/>
+  </div>) : null
 }
 
 export default App
